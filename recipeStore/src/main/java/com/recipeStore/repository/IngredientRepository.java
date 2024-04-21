f8a43651d514d937a05f963287fdf49486dcee8d
Apoorva Raj <92389929+Apoorva-Raj-2520@users.noreply.github.com>
2024-04-21 19:07:07 +0530

    Adding IngredientRepository

---
+++ recipeStore/src/main/java/com/recipeStore/repository/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.recipeStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.recipeStore.entity.Ingredient;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient,Integer>  {
+
+}
