104a49b5c33051557548cb8d4dcb29dca6c9c280
V-A123 <98726654+V-A123@users.noreply.github.com>
2024-04-21 20:26:03 +0530

    Add files via upload
    repository for ingredients

---
+++ recipeStore/src/main/java/com/recipeStore/entity/Ingredient.java
@@ -0,0 +1,53 @@
+package com.recipeStore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ingredient {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String qty;
+	private String price;
+	public Ingredient(int id, String name, String qty, String price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.qty = qty;
+		this.price = price;
+	}
+	public Ingredient() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getQty() {
+		return qty;
+	}
+	public void setQty(String qty) {
+		this.qty = qty;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+}
