3dbdbc15a8addd417fb472ff9e42a57b9f3107f7
Anvesha Nayak <anveshalnayak@gmail.com>
2024-04-22 10:25:26 +0530

    updated Application file

--- recipeStore/src/main/java/com/recipeStore/RecipeStoreApplication.java
+++ recipeStore/src/main/java/com/recipeStore/RecipeStoreApplication.java
@@ -1,5 +1,13 @@
 package com.recipeStore;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RecipeStoreApplication {
-    
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeStoreApplication.class, args);
+	}
+
 }
